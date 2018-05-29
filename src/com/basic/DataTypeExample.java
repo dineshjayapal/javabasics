69803f1ef511c214f3235c27221a6374f6e93ae8
dineshjayapal <dineshjayapal97@gmail.com>
2018-05-29 03:12:23 -0700

    add two string

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void  main(String args[])
+	{
+		String first="hello";
+		String second="world";
+		System.out.println(first+second);
+	}
+}
